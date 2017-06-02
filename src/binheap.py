cd76fc3f8f039b72d1de2ce21e6162fade494cfe
W. Ely Paysinger <paysinger@gmail.com>
2017-06-02 08:46:51 -0700

    pop working?

--- src/binheap.py
+++ src/binheap.py
@@ -6,7 +6,7 @@ class Binheap(list):
 
     def __init__(self,  iterable=None):
         """Initalize with or without iterable."""
-        if iterable and self.checkiterable(iterable):
+        if iterable and self.check_iterable(iterable):
                 for num in iterable:
                     self.push(num)
         else:
@@ -15,12 +15,20 @@ class Binheap(list):
     def push(self, val):
         """."""
         list.append(self, val)
-        self.bubbleUp(len(self))
+        self.bubble_up(len(self))
 
-    def bubbleUp(self, indexPlusOne):
+    def pop(self):
+        """."""
+        temp = self[0]
+        self[0], self[-1] = self[-1], temp
+        temp = list.pop(self, -1)
+        self.bubbule_down()
+        return temp
+
+    def bubble_up(self, index_plus_one):
         """."""
         print('bubbling')
-        i = indexPlusOne - 1
+        i = index_plus_one - 1
         while i // 2 >= 0:
             print('bubbling while ')
             if self[i] < self[i // 2]:
@@ -30,15 +38,27 @@ class Binheap(list):
                 break
             i = i // 2
 
-    def bubbuleDown(self):
+    def bubbule_down(self):
         """."""
-        pass
+        i = 0
+        while i * 2 <= len(self) - 1:
+            i_smallest_child = self.get_smallest_childs_index(i)
+            if self[i] > self[i_smallest_child]:
+                temp = self[i]
+                self[i], self[i_smallest_child] = self[i_smallest_child], temp
+            i = i_smallest_child
 
-    def getSmallestChildsIndex(self):
+    def get_smallest_childs_index(self, i):
         """."""
-        pass
+        if i * 2 + 2 > len(self) - 1:
+            return i * 2 + 1
+        else:
+            if self[i * 2 + 1] < self[i * 2 + 2]:
+                return i * 2 + 1
+            else:
+                return i * 2 + 2
 
-    def checkiterable(self, iterable):
+    def check_iterable(self, iterable):
         """."""
         allnumeric = True
         for item in iterable:
