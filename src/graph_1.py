c3fec7c7a61e15137923d9686348703337962077
Casey O'Kane <okanecasey@gmail.com>
2017-06-04 14:06:40 -0700

    added a new test and fixed del node function.

--- src/graph_1.py
+++ src/graph_1.py
@@ -1,4 +1,4 @@
-"""Module implements a graph data strcture"""
+"""Module implements a graph data strcture."""
 
 
 class Graph(dict):
@@ -27,31 +27,34 @@ class Graph(dict):
             self.setdefault(val, [])
 
     def add_edge(self, val1, val2):
-        """Adds a new edge to the graph.
+        """Add a new edge to the graph.
 
-         connecting the node containing 'val1' and the node containing 'val2'.
-         If either val1 or val2 are not already present in the graph,
-         they should be added. If an edge already exists, overwrite it.
-         """
+        connecting the node containing 'val1' and the node containing 'val2'.
+        If either val1 or val2 are not already present in the graph,
+        they should be added. If an edge already exists, overwrite it.
+        """
         self.setdefault(val1, [])
         self.setdefault(val2, [])
         if val2 not in self[val1]:
             self[val1].append(val2)
 
     def del_node(self, val):
-        """Deletes the node containing 'val' from the graph;
+        """Delete the node containing 'val' from the graph.
 
-         raises an error if no such node exists.
-         """
+        Raises an error if no such node exists.
+        """
         try:
             del self[val]
+            for key in self:
+                if val in self[key]:
+                    self[key].remove(val)
         except KeyError:
             raise ValueError('Value not in graph')
 
     def del_edge(self, val1, val2):
-        """Deletes the edge connecting 'val1' and 'val2' from the graph;
+        """Delete the edge connecting 'val1' and 'val2' from the graph.
 
-        raises an error if no such edge exists.
+        Raises an error if no such edge exists.
         """
         try:
             self[val1].remove(val2)
@@ -63,17 +66,21 @@ class Graph(dict):
         return val in self
 
     def neighbors(self, val):
-        """Returns the list of all nodes connected to the node containing
-        'val' by edges; raises an error if val is not in the graph."""
+        """Return the list of all nodes connected to the node containing.
+
+        'Val' by edges; raises an error if val is not in the graph.
+        """
         if val in self:
             return self[val]
         else:
             raise ValueError('Value not in graph')
 
     def adjacent(self, val1, val2):
-        """Returns True if there is an edge connecting val1 and val2,
-        False if not; raises an error if either of the supplied
-        values are not in g."""
+        """Return True if there is an edge connecting val1 and val2.
+
+        False if not; raises an error if either of the supplied.
+        values are not in g.
+        """
         if val1 not in self or val2 not in self:
             raise ValueError('Node not found.')
         if val2 in self[val1]:
--- src/test_graph_1.py
+++ src/test_graph_1.py
@@ -5,7 +5,7 @@ import pytest
 @pytest.fixture
 def empty_graph():
     """Create an empty graph."""
-    from graph import Graph
+    from graph_1 import Graph
     return Graph()
 
 
@@ -23,7 +23,7 @@ def tri_graph(empty_graph):
 
 
 def test_init_creates_empty_graph(empty_graph):
-        """."""
+        """Initialize and empty graph."""
         assert empty_graph.nodes() == []
 
 
@@ -45,15 +45,21 @@ def test_add_node_adds_node(empty_graph):
 
 
 def test_add_edge_adds_an_edge(tri_graph):
-    """Adds a new edge to the graph."""
+    """Add a new edge to the graph."""
     tri_graph.add_edge(3, 2)
     assert tri_graph.neighbors(3) == [1, 2]
 
 
 def test_del_node_removes_a_node(tri_graph):
-    """Deletes the node containing 'val' from the graph."""
+    """Delete the node containing 'val' from the graph."""
     tri_graph.del_node(3)
     assert tri_graph.nodes() == [1, 2]
+
+
+def test_del_node_3_and_check_two_has_no_neighbors(tri_graph):
+    """Delete the node containing 3 and check that 2 has no neighbors."""
+    tri_graph.del_node(3)
+    assert tri_graph.neighbors(2) == []
 
 
 def test_del_node_raises_ValueError_when_node_not_in_graph(tri_graph):
@@ -63,7 +69,7 @@ def test_del_node_raises_ValueError_when_node_not_in_graph(tri_graph):
 
 
 def test_del_edge_removes_edge_from_node(tri_graph):
-    """Deletes the edge connecting 'val1' and 'val2' from the graph."""
+    """Delete the edge connecting 'val1' and 'val2' from the graph."""
     tri_graph.del_edge(1, 2)
     assert tri_graph.neighbors(1) == []
 
@@ -81,25 +87,25 @@ def test_has_node_returns_true_or_false_if_node_present_or_not(tri_graph):
 
 
 def test_neighbors_returns_list_of_neighbors(tri_graph):
-    """Returns the list of all nodes connected to the node."""
+    """Return the list of all nodes connected to the node."""
     tri_graph.add_edge(3, 2)
     assert tri_graph.neighbors(3) == [1, 2]
 
 
 def test_neighbors_raises_ValueError_if_node_not_in_graph(tri_graph):
-    """Returns the list of all nodes connected to the node."""
+    """Return the list of all nodes connected to the node."""
     with pytest.raises(ValueError):
         tri_graph.neighbors(4)
 
 
 def test_adjacent_returns_true_or_false_if_node_has_the_edge_not(tri_graph):
-    """Returns True if there is an edge connecting val1 and val2."""
+    """Return True if there is an edge connecting val1 and val2."""
     assert tri_graph.adjacent(1, 2) is True
     assert tri_graph.adjacent(1, 3) is False
 
 
 def test_adjacent_raises_ValueError_if_either_are_not_in_graph(tri_graph):
-    """Raises an error if either of the supplied values are not in graph."""
+    """Raise an error if either of the supplied values are not in graph."""
     with pytest.raises(ValueError):
         tri_graph.adjacent(1, 4)
     with pytest.raises(ValueError):
