7109dfa06366cbac682b277fcf7251e651b08929
W. Ely Paysinger <paysinger@gmail.com>
2017-06-20 08:03:01 -0700

    Lots of test written!

---
+++ src/bst.py
@@ -0,0 +1,13 @@
+"""Implements Binary Search Tree data structure."""
+
+
+class Bst(dict):
+    """Create a binay search tree data strcture modeled off a dictionary."""
+
+    def __init__(self):
+        """Inialize probably wont need anthing."""
+
+if __name__ == '__main__':
+    # import timeit
+    # print(timeit.timeit("test()", setup="from __main__ import test"))
+    pass
