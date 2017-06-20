3bdd4f8ba8d51efd10bb2620c3d2dc4b0b671f54
W. Ely Paysinger <paysinger@gmail.com>
2017-06-20 08:44:27 -0700

    basic layout for bst created.

--- src/bst.py
+++ src/bst.py
@@ -1,11 +1,90 @@
 """Implements Binary Search Tree data structure."""
 
 
-class Bst(dict):
-    """Create a binay search tree data strcture modeled off a dictionary."""
+class Node(object):
+    """Node data structure."""
 
-    def __init__(self):
-        """Inialize probably wont need anthing."""
+    def __init__(self, value):
+        """Init a node."""
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class Bst(object):
+    """Create a binay search tree data structure."""
+
+    def __init__(self,  iterable=None):
+        """Init with or without iterable but only nums."""
+        if not iterable:
+            pass
+        elif iterable and self.check_iterable(iterable):
+            self.head = None
+            self.length = 0
+            self.depth = 0
+            for num in iterable:
+                self.insert(num)
+        else:
+            raise TypeError('Must be iterable and all numbers.')
+
+    def check_iterable(self, iterable):
+        """Check iterable for all numeric values."""
+        allnumeric = True
+        for item in iterable:
+            if type(item) not in [int, float]:
+                allnumeric = False
+        return allnumeric
+
+    def insert(self, val):
+        """Insert the value val into the BST.
+
+        If val is already present, it will be ignored.
+        """
+        if self.check_iterable([val]):
+
+            pass
+        else:
+            raise TypeError('Must be a number.')
+
+    def search(self, val):
+        """Return the node containing that value, else None."""
+        pass
+
+    def size(self):
+        """Rreturn the integer size of the BST.
+
+        (equal to the total number of values stored in the tree).
+        It will return 0 if the tree is empty.
+        """
+        pass
+
+    def depth(self):
+        """Return an integer representing the total number of levels in tree.
+
+        If there are no values, depth is 0, if one value the depth should be 1,
+        if two values it will be 2, if three values it may be 2 or 3.
+        """
+        pass
+
+    def contains(self, val):
+        """Return True if val is in the BST, False if not."""
+        pass
+
+    def balance(self):
+        """Return an integer, positive, negative or zero.
+
+        that represents how well balanced the tree is.
+        Trees which are higher on the left than the right should return a
+        positive value, trees which are higher on the right than the left
+        should return a negative value. An ideally-balanced tree should
+        return 0.
+        """
+        pass
+
+    def __len__(self):
+        """Return the length."""
+        return self.size()
+
 
 if __name__ == '__main__':
     # import timeit
--- src/test_bst.py
+++ src/test_bst.py
@@ -12,7 +12,7 @@ def bst_empty():
 
 @pytest.fixture
 def bst_left_balance():
-    """Create a binary search tree 5 numbers snake.
+    r"""Create a binary search tree 5 numbers snake.
 
                     5
                    /
@@ -32,7 +32,7 @@ def bst_left_balance():
 
 @pytest.fixture
 def bst_balanced():
-    """Create a binary search tree 5 numbers.
+    r"""Create a binary search tree 5 numbers.
 
                      5
                    /   \
@@ -48,7 +48,7 @@ def bst_balanced():
 
 @pytest.fixture
 def bst_right_balance():
-    """Create a binary search tree 5 numbers.
+    r"""Create a binary search tree 5 numbers.
 
                      5
                    /   \
@@ -74,10 +74,16 @@ def bst_100_rand():
 
 
 def test_initalizing_with_non_iterable_or_not_numbers_raises_ValueError():
-    """Init returns Value error with with non-numbers or non-iterables"""
+    """Init returns Value error with with non-numbers or non-iterables."""
     from bst import Bst
     with pytest.raises(TypeError):
         Bst("dfsdfadgasdg")
+
+
+def test_insert_must_be_a_number(bst_empty):
+    """Raise TypeError on non number insert."""
+    with pytest.raises(TypeError):
+        bst_empty.insert("dfsdfadgasdg")
 
 
 def test_insert_to_empty_tree_increases_tree_length(bst_empty):
@@ -86,120 +92,120 @@ def test_insert_to_empty_tree_increases_tree_length(bst_empty):
     assert len(bst_empty) == 1
 
 
-def test_insert_adds_value_to_tree(bst_balanced):
-    """Value added to tree."""
-    bst_balanced.insert(15)
-    assert bst.contains(15) is True
-    assert bst.search(15).value == 15
-
-
-def test_insert_will_not_duplicate_value(bst_balanced):
-    """Value not added twice."""
-    bst_balanced.insert(6)
-    assert bst_balanced.size() == 6
-
-
-def test_insert_to_balanced_tree_changes_balance(bst_balanced):
-    """Balance changes."""
-    assert bst_balanced.balance() == 0
-    bst_balanced.insert(4)
-    assert bst_balanced.balance() == 1
-
-
-def test_search_finds_node(bst_balanced):
-    """Search returns node with value."""
-    assert bst_balanced.search(1).value == 1
-
-
-def test_search_returns_none_when_value_not_in_tree(bst_balanced):
-    """Search returns None."""
-    assert bst_balanced.search(25) is None
-
-
-def test_size_is_correct_on_empty_tree(bst_empty):
-    """Tree size is accurate."""
-    assert bst_empty.size() == 0
-
-
-def test_size_is_correct_on_filled_tree(bst_100_rand):
-    """Tree size is accurate."""
-    assert bst_empty.size() == 100
-
-
-def test_depth_returns_zero_on_empty_tree(bst_empty):
-    """Return 0 on empty tree."""
-    assert bst_empty.depth() == 0
-
-
-def test_depth_returns_correct_value_balanced_tree(bst_balanced):
-    """Return value on tree."""
-    assert bst_balanced.depth() == 3
-
-
-def test_depth_returns_correct_value_right_balanced_tree(bst_right_balance):
-    """Return value on empty tree."""
-    assert bst_right_balanced.depth() == 4
-
-
-def test_depth_returns_correct_value_left_balanced_tree(bst_left_balance):
-    """Return value on empty tree."""
-    assert bst_left_balanced.depth() == 5
-
-
-def test_contains_returns_false_on_empty_tree(bst_empty):
-    """False on empty tree."""
-    assert bst_empty.contains(4) is False
-
-
-def test_contains_returns_false_on_balanced_tree(bst_balanced):
-    """False on balanced tree."""
-    assert bst_balanced.contains(25) is False
-
-
-def test_contains_returns_false_on_right_balanced_tree(bst_right_balance):
-    """False on right balanced tree."""
-    assert bst_right_balance.contains(25) is False
-
-
-def test_contains_returns_false_on_left_balanced_tree(bst_left_balance):
-    """False on left balanced tree."""
-    assert bst_left_balance.contains(25) is False
-
-
-def test_contains_returns_true_on_tree_with_value_left(bst_left_balance):
-    """Tree has value true."""
-    assert bst_left_balance.contains(3) is True
-    assert bst_left_balance.contains(1) is True
-    assert bst_left_balance.contains(2) is True
-
-
-def test_contains_returns_true_on_tree_with_value_right(bst_right_balance):
-    """Tree has value true."""
-    assert bst_right_balance.contains(6) is True
-    assert bst_right_balance.contains(2) is True
-
-
-def test_contains_returns_true_on_tree_with_value(bst_balance):
-    """Tree has value true."""
-    assert bst_balance.contains(6) is True
-    assert bst_balance.contains(3) is True
-
-
-def test_balance_right_tree(bst_right_balance):
-    """Tree balanced right returns -1."""
-    assert bst_right_balance.balance() == -1
-
-
-def test_balance_left_tree(bst_left_balance):
-    """Tree balanced right returns 1."""
-    assert bst_left_balance.balance() == 1
-
-
-def test_balance_balanced_tree(bst_balanced):
-    """Tree balanced right returns -1."""
-    assert bst_balanced.balance() == 0
-
-
-def test_balance_empty_tree(bst_empty):
-    """Tree balanced right returns 0."""
-    assert bst_empty.balance() == 0
+# def test_insert_adds_value_to_tree(bst_balanced):
+#     """Value added to tree."""
+#     bst_balanced.insert(15)
+#     assert bst_balanced.contains(15) is True
+#     assert bst_balanced.search(15).value == 15
+#
+#
+# def test_insert_will_not_duplicate_value(bst_balanced):
+#     """Value not added twice."""
+#     bst_balanced.insert(6)
+#     assert bst_balanced.size() == 6
+#
+#
+# def test_insert_to_balanced_tree_changes_balance(bst_balanced):
+#     """Balance changes."""
+#     assert bst_balanced.balance() == 0
+#     bst_balanced.insert(4)
+#     assert bst_balanced.balance() == 1
+#
+#
+# def test_search_finds_node(bst_balanced):
+#     """Search returns node with value."""
+#     assert bst_balanced.search(1).value == 1
+#
+#
+# def test_search_returns_none_when_value_not_in_tree(bst_balanced):
+#     """Search returns None."""
+#     assert bst_balanced.search(25) is None
+#
+#
+# def test_size_is_correct_on_empty_tree(bst_empty):
+#     """Tree size is accurate."""
+#     assert bst_empty.size() == 0
+#
+#
+# def test_size_is_correct_on_filled_tree(bst_100_rand):
+#     """Tree size is accurate."""
+#     assert bst_empty.size() == 100
+#
+#
+# def test_depth_returns_zero_on_empty_tree(bst_empty):
+#     """Return 0 on empty tree."""
+#     assert bst_empty.depth() == 0
+#
+#
+# def test_depth_returns_correct_value_balanced_tree(bst_balanced):
+#     """Return value on tree."""
+#     assert bst_balanced.depth() == 3
+#
+#
+# def test_depth_returns_correct_value_right_balanced_tree(bst_right_balance):
+#     """Return value on empty tree."""
+#     assert bst_right_balance.depth() == 4
+#
+#
+# def test_depth_returns_correct_value_left_balanced_tree(bst_left_balance):
+#     """Return value on empty tree."""
+#     assert bst_left_balance.depth() == 5
+#
+#
+# def test_contains_returns_false_on_empty_tree(bst_empty):
+#     """False on empty tree."""
+#     assert bst_empty.contains(4) is False
+#
+#
+# def test_contains_returns_false_on_balanced_tree(bst_balanced):
+#     """False on balanced tree."""
+#     assert bst_balanced.contains(25) is False
+#
+#
+# def test_contains_returns_false_on_right_balanced_tree(bst_right_balance):
+#     """False on right balanced tree."""
+#     assert bst_right_balance.contains(25) is False
+#
+#
+# def test_contains_returns_false_on_left_balanced_tree(bst_left_balance):
+#     """False on left balanced tree."""
+#     assert bst_left_balance.contains(25) is False
+#
+#
+# def test_contains_returns_true_on_tree_with_value_left(bst_left_balance):
+#     """Tree has value true."""
+#     assert bst_left_balance.contains(3) is True
+#     assert bst_left_balance.contains(1) is True
+#     assert bst_left_balance.contains(2) is True
+#
+#
+# def test_contains_returns_true_on_tree_with_value_right(bst_right_balance):
+#     """Tree has value true."""
+#     assert bst_right_balance.contains(6) is True
+#     assert bst_right_balance.contains(2) is True
+#
+#
+# def test_contains_returns_true_on_tree_with_value(bst_balance):
+#     """Tree has value true."""
+#     assert bst_balance.contains(6) is True
+#     assert bst_balance.contains(3) is True
+#
+#
+# def test_balance_right_tree(bst_right_balance):
+#     """Tree balanced right returns -1."""
+#     assert bst_right_balance.balance() == -1
+#
+#
+# def test_balance_left_tree(bst_left_balance):
+#     """Tree balanced right returns 1."""
+#     assert bst_left_balance.balance() == 1
+#
+#
+# def test_balance_balanced_tree(bst_balanced):
+#     """Tree balanced right returns -1."""
+#     assert bst_balanced.balance() == 0
+#
+#
+# def test_balance_empty_tree(bst_empty):
+#     """Tree balanced right returns 0."""
+#     assert bst_empty.balance() == 0
