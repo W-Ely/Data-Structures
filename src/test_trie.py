340daea6869e35accdead482f4dbb5b5d5df6378
W. Ely Paysinger <paysinger@gmail.com>
2017-07-07 22:01:24 -0700

    working on trie.

--- src/test_trie.py
+++ src/test_trie.py
@@ -1,1 +1,21 @@
 """Test module for trie data structure."""
+import pytest
+from trie import Trie
+
+
+@pytest.fixture
+def trie():
+    """Create empty tree for fixture."""
+    return Trie()
+
+
+def test_insert_inserts_new_string(trie):
+    """Test insert adds new string."""
+    trie.insert('abc')
+    assert trie.contains('abc') is True
+
+
+# insert(self, string): will insert the input string into the trie. If character in the input string is already present, it will be ignored.
+# contains(self, string): will return True if the string is in the trie, False if not.
+# size(self): will return the total number of words contained within the trie. 0 if empty.
+# remove(self, string): will remove the given string from the trie. If the word doesn’t exist, will raise an appropriate exception.
--- src/trie.py
+++ src/trie.py
@@ -4,31 +4,64 @@
 class Trie(dict):
     """Create Trie data structure."""
 
-    def __init__(self):
-        """Init a trie."""
-        pass
+    def __init__(self, string=None):
+        """Inialize trie."""
+        self._length = 0
+        if string and type(string) is str:
+            self.insert(string)
 
     def insert(self, string):
-        """Insert the input string into the trie.
+        """Insert the string into the trie.
 
-        If char in the input string is already present, it will be ignored.
+        If char in the string is already present, it will be ignored.
         """
-        pass
+        self._length += 1
+        if len(string) == 1:
+            self.setdefault(string[0], "$")
+        else:
+            self.setdefault(string[0], Trie(string[1:]))
 
     def contains(self, string):
         """Return True if the string is in the trie, False if not."""
-        pass
+        temp = self
+        try:
+            for char in string:
+                temp = temp[char]
+            return temp == '$'
+        except KeyError:
+            return False
 
     def size(self):
         """Return the total number of words contained within the trie.
 
         0 if empty.
         """
-        pass
+        return self._length
 
     def remove(self, string):
         """Remove the given string from the trie.
 
         If the word doesn't exist, will raise an appropriate exception.
         """
-        pass
+        temp = self
+        try:
+            for char in string:
+                if len(temp[char].keys()) > 1:
+                    temp = temp[char]
+                else:
+                    del temp[char]
+                    self._length -= 1
+                    return
+        except KeyError:
+            raise KeyError("Value on it trie.")
+
+
+trie = Trie('test')
+print(trie)
+print(trie.contains('test'))  # True
+print(trie.contains('no'))  # False
+print(trie.size())  # 1
+trie.remove('test')
+print(trie.size())  # 0
+print(trie.contains('test'))  # False
+print(trie)
