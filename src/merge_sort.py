49f6295672ea22cca1a196abf5323777122a4c53
Casey O'Kane <okanecasey@gmail.com>
2017-07-12 15:37:53 -0700

    Merge sort complete, could use more tests.

---
+++ src/merge_sort.py
@@ -0,0 +1,56 @@
+"""Merge sort data structure implementation."""
+
+
+def merge(left, right):
+    """Combine lists in correct order."""
+    if not left or not right:
+        return left or right
+    results = []
+    i, j = 0, 0
+    while len(results) < len(left) + len(right):
+        if left[i] < right[j]:
+            results += [left[i]]
+            i += 1
+        else:
+            results += [right[j]]
+            j += 1
+        if i == len(left) or j == len(right):
+            results += left[i:] or right[j:]
+            return results
+
+
+def merge_sort(numbers):
+    """Recursively split list and returns merged list."""
+    if len(numbers) <= 1:
+        return numbers
+    middle_idx = int(len(numbers) / 2)
+    left = merge_sort(numbers[:middle_idx])
+    right = merge_sort(numbers[middle_idx:])
+    return merge(left, right)
+
+
+if __name__ == '__main__':  # pragma: no cover
+    from timeit import Timer
+    best = Timer(
+        'merge_sort([x for x in range(100)])',
+        "from __main__ import merge_sort"
+    )
+    worst = Timer(
+        'merge_sort([x for x in range(100)][::-1])',
+        "from __main__ import merge_sort; from random import randint"
+    )
+    random = Timer(
+        'merge_sort([randint(0, 1000) for x in range(100)][::-1])',
+        "from __main__ import merge_sort; from random import randint"
+    )
+    print("""
+""")
+    print("#================= best case search 10000x ==============#")
+    print(best.timeit(number=1000))
+    print('')
+    print("#================= worse case search 10000x==============#")
+    print(worst.timeit(number=1000))
+    print('')
+    print("#================= random case search 10000x=============#")
+    print(worst.timeit(number=1000))
+    print('')
---
+++ src/test_merge_sort.py
@@ -0,0 +1,70 @@
+"""Test Module for Bubble sort."""
+from merge_sort import merge_sort
+import pytest
+from random import randint
+
+
+@pytest.fixture
+def short_list():
+    """Create short list."""
+    return [randint(1, 1000) for _ in range(100)]
+
+
+@pytest.fixture
+def sorted_list():
+    """Create short list."""
+    return [x for x in range(100)]
+
+
+@pytest.fixture
+def long_and_large():
+    """Create long list."""
+    return [randint(-100, 100000) for _ in range(1000)]
+
+
+def test_empty_list_returns_empty_list():
+    """Empty list retruns None."""
+    assert merge_sort([]) == []
+
+
+def test_merge_sorts(long_and_large):
+    """Test bubble sorts a long large list."""
+    result = merge_sort(long_and_large)
+    long_and_large.sort()
+    assert long_and_large == result
+
+
+def test_merge_sorts_short(short_list):
+    """Test bubble sort on short list."""
+    result = merge_sort(short_list)
+    short_list.sort()
+    assert result == short_list
+
+
+def test_merge_sort_perfectly_backwards_list(sorted_list):
+    """Test with sorted list."""
+    result = merge_sort(sorted_list)
+    sorted_list.sort()
+    assert result == sorted_list
+
+
+def test_merge_sorted_perfectly_forward_list(sorted_list):
+    """Tests best case."""
+    result = merge_sort(sorted_list[::-1])
+    sorted_list.sort()
+    assert result == sorted_list
+
+
+def test_merge_sorted_multiple_duplicates(short_list):
+    """Tests best case."""
+    result = merge_sort(short_list[::-1] + short_list)
+    short_list += short_list
+    short_list.sort()
+    assert result == short_list
+
+
+def test_bubbles():
+    """Another quick test."""
+    sorted_list = [99, 22, 55, 4, 66, 87, 23, 11]
+    sorted_list.sort()
+    assert merge_sort([99, 22, 55, 4, 66, 87, 23, 11]) == sorted_list
