da3eaf029a795a603702ccf98c4ff158e5ea8e97
W. Ely Paysinger <paysinger@gmail.com>
2017-07-15 15:14:28 -0700

    radix

---
+++ src/radix_sort.py
@@ -0,0 +1,44 @@
+"""Radix Sort Implementation."""
+
+
+def radix_sort(numbers):
+    """Sort some numbers."""
+    modulus = 10
+    divisor = 1
+    while True:
+        container = [[] for _ in range(10)]
+        for num in numbers:
+            idx = int(num % modulus / divisor)
+            container[idx].append(num)
+        divisor, modulus = modulus, modulus * 10
+
+        if len(container[0]) == len(numbers):
+            return container[0]
+
+        numbers = []
+        for numerals in container:
+            for num in numerals:
+                numbers.append(num)
+
+
+if __name__ == '__main__':  # pragma: no cover
+    from timeit import Timer
+    best = Timer(
+        'radix_sort([x for x in range(100)])',
+        "from __main__ import radix_sort"
+    )
+    worst = Timer(
+        'radix_sort([x for x in range(100)][::-1])',
+        "from __main__ import radix_sort; from random import randint"
+    )
+    print("""
+Quicksort (sometimes called partition-exchange sort) is an efficient sorting
+algorithm, serving as a systematic method for placing the elements of an
+array in order.
+""")
+    print("#================= best case search 10000x ==============#")
+    print(best.timeit(number=1000))
+    print('')
+    print("#================= worse case search 10000x==============#")
+    print(worst.timeit(number=1000))
+    print('')
---
+++ src/test_radix_sort.py
@@ -0,0 +1,81 @@
+"""Test Module for Bubble sort."""
+import pytest
+from radix_sort import radix_sort
+from random import randint
+
+
+@pytest.fixture
+def short_list():
+    """Create short list."""
+    return [randint(1, 1000) for _ in range(100)]
+
+
+@pytest.fixture
+def sorted_list():
+    """Create short list."""
+    return [x for x in range(100)]
+
+
+@pytest.fixture
+def long_and_large():
+    """Create long list."""
+    return [randint(-100, 100000) for _ in range(1000)]
+
+
+def test_empty_list_returns_empty_list():
+    """Empty list retruns None."""
+    assert radix_sort([]) == []
+
+
+def test_radix_sorts(long_and_large):
+    """Test radix_sorts a long large list."""
+    result = radix_sort(long_and_large)
+    long_and_large.sort()
+    assert long_and_large == result
+
+
+def test_radix_sorts_short(short_list):
+    """Test radix_sort on short list."""
+    result = radix_sort(short_list)
+    short_list.sort()
+    assert result == short_list
+
+
+def test_radix_sort_perfectly_backwards_list(sorted_list):
+    """Test with sorted list."""
+    result = radix_sort(sorted_list)
+    sorted_list.sort()
+    assert result == sorted_list
+
+
+def test_radix_sorted_perfectly_forward_list(sorted_list):
+    """Test best case."""
+    result = radix_sort(sorted_list[::-1])
+    sorted_list.sort()
+    assert result == sorted_list
+
+
+def test_radix_sorted_multiple_duplicates(short_list):
+    """Test best case."""
+    result = radix_sort(short_list[::-1] + short_list)
+    short_list += short_list
+    short_list.sort()
+    assert result == short_list
+
+
+def test_radixs():
+    """Another radix test."""
+    sorted_list = [99, 22, 55, 4, 66, 87, 23, 11]
+    sorted_list.sort()
+    assert radix_sort([99, 22, 55, 4, 66, 87, 23, 11]) == sorted_list
+
+
+def test_odd_list():
+    """Making sure the radix sort works with an odd list."""
+    numbers = [17, 11, 14, 99, 20]
+    assert radix_sort(numbers) == sorted(numbers)
+
+
+def test_single_item_in_list():
+    """When there is only one value, what will it do."""
+    assert radix_sort([1]) == [1]
